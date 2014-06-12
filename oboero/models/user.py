e0bd740062d3c271b50480034cf1a6b0037251f6
Ariyasuk Kittikorn <ariyasuk-k@klab.com>
2014-06-12 22:04:32 +0900

    datamodel for pic

--- oboero/models/user.py
+++ oboero/models/user.py
@@ -4,6 +4,7 @@ from oboero import db
 class User(db.Model):
     email = db.Column(db.String(120), primary_key=True)
     username = db.Column(db.String(64), unique=True)
+    pic_url = db.Column(db.String(64))
 
     def __init__(self, email, username):
         self.email = email
