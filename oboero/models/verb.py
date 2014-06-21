2457e9ddb28fa54fbb6736fd54dd088d3d2d3293
tay-k <tay-k@klab.com>
2014-06-21 16:24:27 +0900

    add model for verb question, redo server logic

--- oboero/models/verb.py
+++ oboero/models/verb.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from oboero import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 class Verb(db.Model):
     __tablename__ = "verb"
@@ -12,3 +14,17 @@ class Verb(db.Model):
     conditional = db.Column(db.String(25), nullable=False)  # ikeba
     passive = db.Column(db.String(25), nullable=False)  # ikareru
     causative = db.Column(db.String(25), nullable=False)  # ikaseru
+
+    questions = relationship("VerbQuestion", backref="verb")
+
+class VerbQuestion(db.Model):
+    __tablename__ = "verb_question"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    teinei = db.Column(db.String(25), nullable=True)  # kobini ni (ikimasu).
+    casual = db.Column(db.String(25), nullable=True)  # masugu (iku) to, kobini ga miemasu.
+    te = db.Column(db.String(25), nullable=True)  # itsu (itte) tara ii desuka?
+    potential = db.Column(db.String(25), nullable=True)  # nanji kara (ikeru)?
+    conditional = db.Column(db.String(25), nullable=True)  # doko ni (ikeba) ii desuka?
+    passive = db.Column(db.String(25), nullable=True)  # robot ga (ikare).
+    causative = db.Column(db.String(25), nullable=True)  # kare wa watashi ni jimu wo (ikaseru)
+    verb_id = db.Column(db.Integer, ForeignKey('verb.id'))
--- oboero/views/verb.py
+++ oboero/views/verb.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import render_template, jsonify, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response
 from flask.blueprints import Blueprint
-import random, json, urllib2
 from .. import db
-from oboero.models.verb import Verb
+from oboero.models.verb import Verb, VerbQuestion
 from oboero.services.verb_service import VerbService
 blueprint = Blueprint('verb', __name__)
 
@@ -21,16 +20,40 @@ def add():
     return render_template('verb/add.html')
 
 
+@blueprint.route('/conjugate')
+def conjugate():
+    verb_service = VerbService()
+    teinei = request.args.get('teinei')
+    group = request.args.get('group', type=int)
+    teinei, casual, te, potential, conditional, passive, causative = \
+        verb_service.get_forms(teinei, group)
+
+    verb = {'teinei': teinei, 'casual': casual, 'te': te,
+            'potential': potential, 'conditional': conditional,
+            'passive': passive, 'causative': causative}
+
+    response = make_response(verb, 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
 @blueprint.route('/new', methods=['POST'])
 def new():
-    verb_service = VerbService()
-    teinei = request.form.get('teinei')
-    group = request.form.get('group', type=int)
-    teinei, casual, te, potential, conditional, passive, causative = verb_service.get_forms(teinei, group)
-    print(conditional)
+    teinei, q_teinei = request.form.get('teinei'), request.form.get('q_teinei')
+    casual, q_casual = request.form.get('casual'), request.form.get('q_casual')
+    te, q_te = request.form.get('te'), request.form.get('q_te')
+    potential, q_potential = request.form.get('potential'), request.form.get('q_potential')
+    conditional, q_conditional = request.form.get('conditional'), request.form.get('q_conditional')
+    passive, q_passive = request.form.get('passive'), request.form.get('q_passive')
+    causative, q_causative = request.form.get('causative'), request.form.get('q_causative')
 
     verb = Verb(teinei=teinei, casual=casual, te=te, potential=potential,
                 conditional=conditional, passive=passive, causative=causative)
+
+    question = VerbQuestion(teinei=q_teinei, casual=q_casual, te=q_te, potential=q_potential,
+                            conditional=q_conditional, passive=q_passive, causative=q_causative)
+
+    verb.questions.append(question)
     db.session.add(verb)
     db.session.commit()
     return redirect(url_for('verb.list'))
